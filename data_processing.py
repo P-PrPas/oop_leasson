6e3275fe23c9400a1d66df051360d5e24390ad71
Peerapas CHINNAPATPAKDEEKUL <peerapas68@gmail.com>
2024-11-09 12:03:24 +0700

    Modify data_processing.py in procedural style

--- data_processing.py
+++ data_processing.py
@@ -17,36 +17,63 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
 
 # Print the average temperature of all the cities
 print("The average temperature of all the cities:")
-temps = []
+italy_temps = []
 for city in cities:
-    temps.append(float(city['temperature']))
-print(sum(temps)/len(temps))
+    italy_temps.append(float(city['temperature']))
+print(sum(italy_temps)/len(italy_temps))
 print()
 
 # Print all cities in Italy
-temps = []
+italy_temps = []
 my_country = 'Italy'
 for city in cities:
     if city['country'] == my_country:
-        temps.append(city['city'])
+        italy_temps.append(city['city'])
 print("All the cities in", my_country, ":")
-print(temps)
+print(italy_temps)
 print()
 
 # Print the average temperature for all the cities in Italy
 # Write code for me
+italy_temps = []
+my_country = 'Italy'
+for city in cities:
+    if city['country'] == my_country:
+        italy_temps.append(float(city['temperature']))
+print("The average temperature of all the cities in", my_country, ":")
+print(sum(italy_temps)/len(italy_temps))
+print()
 
 # Print the max temperature for all the cities in Italy
 # Write code for me
+italy_temps = []
+my_country = 'Italy'
+for city in cities:
+    if city['country'] == my_country:
+        italy_temps.append(float(city['temperature']))
+print("The max temperature of all the cities in", my_country, ":")
+print(max(italy_temps))
+print()
 
 # Let's write a function to filter out only items that meet the condition
+
 def filter(condition, dict_list):
-    pass
+    filtered_list = []
+    for item in dict_list:
+        if condition(item):
+            filtered_list.append(item)
+    return filtered_list
+x = filter(lambda x: float(x['latitude']) >= 60.0, cities)
+
+for item in x:
+    print(item)
 
 # Let's write a function to do aggregation given an aggregation function and an aggregation key
 def aggregate(aggregation_key, aggregation_function, dict_list):
-    pass
-
+    value = []
+    for item in dict_list:
+        value.append(item[aggregation_key])
+    return aggregation_function(value)
 # Let's write code to
 # - print the average temperature for all the cities in Italy
 # - print the average temperature for all the cities in Sweden
